41a75039074a5b91d2120e529603e631ad000ee9
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-26 15:48:07 +0100

    Seeds and DB route

--- You-Know/bin/seeds.js
+++ You-Know/bin/seeds.js
@@ -1,47 +1,157 @@
-// Seeds file that remove all users and create 2 new users
-
-// To execute this seed, run from the root of the project
-// $ node bin/seeds.js
-
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Game = require('../models/Game');
+const Question = require('../models/Question');
+mongoose.connect(`mongodb://${process.env.DB}`);
 
-const bcryptSalt = 10;
+const passwords = ["y", "z"];
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
 
-mongoose
-  .connect('mongodb://localhost/you-know', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
-let users = [
+const users = [
   {
-    username: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
+    username: "y",
+    password: bcrypt.hashSync(passwords[0], salt),
+    email: "asdasdasdasdasd@gmail.com",
+    imagePath: "https://res.cloudinary.com/yunevk/image/upload/v1543235622/youknow/pocoyo.png.png",
+    status: "Active",
+    confirmationCode: "rliucne4o59439cct3475c4",
+    points: 524,
+    stadistics: {
+      general: {
+        correct: 20,
+        failed: 10
+      },
+      entertainment: {
+        correct: 60,
+        failed: 25
+      },
+      science: {
+        correct: 2,
+        failed: 50
+      },
+      art: {
+        correct: 90,
+        failed: 24
+      },
+      historyAndGeography: {
+        correct: 27,
+        failed: 94
+      },
+      sports: {
+        correct: 46,
+        failed: 67
+      },
+      ironhack: {
+        correct: 32,
+        failed: 42
+      }
+    }
   },
   {
-    username: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
+    username: "z",
+    password: bcrypt.hashSync(passwords[1], salt),
+    email: "dsgsdgdfgdsgresdg@gmail.com",
+    imagePath: "",
+    status: "Active",
+    confirmationCode: "328746sdkhfsdfgewu234bd",
+    points: 200,
+    stadistics: {
+      general: {
+        correct: 12,
+        failed: 10
+      },
+      entertainment: {
+        correct: 20,
+        failed: 10
+      },
+      science: {
+        correct: 33,
+        failed: 42
+      },
+      art: {
+        correct: 21,
+        failed: 45
+      },
+      historyAndGeography: {
+        correct: 11,
+        failed: 14
+      },
+      sports: {
+        correct: 6,
+        failed: 2
+      },
+      ironhack: {
+        correct: 12,
+        failed: 45
+      }
+    }
   }
 ]
 
-User.deleteMany()
-.then(() => {
-  return User.create(users)
-})
-.then(usersCreated => {
-  console.log(`${usersCreated.length} users created with the following id:`);
-  console.log(usersCreated.map(u => u._id));
-})
-.then(() => {
-  // Close properly the connection to Mongoose
-  mongoose.disconnect()
-})
-.catch(err => {
-  mongoose.disconnect()
-  throw err
-})
+const categories = [
+  {
+    name: "general",
+    categoryApiId: [9, 28]
+  },
+  {
+    name: "art",
+    categoryApiId: [25]
+  }
+]
+
+const games = [
+  {
+    answeredQuestions: 5,
+    totalQuestions: 10,
+    difficulty: "easy",
+    points: 5
+  },
+  {
+    answeredQuestions: 25,
+    totalQuestions: 50,
+    difficulty: "hard",
+    points: 125
+  }
+]
+
+const questions = [
+  {
+    category: "ironhack",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Who is the Teach leader in web?",
+    correct_answer: "Vicario",
+    incorrect_answers: ["Gabi", "Juan", "Teo"]
+  },
+  {
+    category: "ironhack",
+    type: "multiple",
+    difficulty: "medium",
+    question: "How much time is full time bootcamp?",
+    correct_answer: "9 weeks",
+    incorrect_answers: ["6 months", "9 months", "10 weeks"]
+  }
+]
+
+User.create(users)
+  .then(() => {
+    console.log(`Created ${users.length} users`)
+    return Category.create(categories)
+  })
+  .then(() => {
+    console.log(`Created ${categories.length} categories`)
+    return Game.create(games)
+  })
+  .then(() => {
+    console.log(`Created ${games.length} games`)
+    return Question.create(questions)
+  })
+  .then(() => {
+    console.log(`Created ${questions.length} questions`)
+    mongoose.connection.close()
+  })
+  .catch(err => console.log(err))
