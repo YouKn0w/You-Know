61d4d1cefd1873032b227e90c7c51495d43067b7
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-26 17:00:08 +0100

    Modified route files

--- You-Know/routes/auth.js
+++ You-Know/routes/auth.js
@@ -10,6 +10,9 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+router.get('/', ensureLoggedOut("/main"), (req, res, next) => {
+  res.render('index');
+});
 
 router.get("/login", (req, res, next) => {
   res.render("auth/login", { "message": req.flash("error") });
