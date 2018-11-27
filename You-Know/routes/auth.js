205d2529170ef5f456bd2737bf3d435e734f1561
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-27 16:15:27 +0100

    Icon generator

--- You-Know/routes/auth.js
+++ You-Know/routes/auth.js
@@ -3,8 +3,23 @@ const passport = require('passport');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 const transporter = require('../mail/transporter');
 const uploadCloud = require('../config/cloudinary');
+const cloudinary = require('cloudinary');
+const jdenticon = require("jdenticon"),
+  fs = require("fs");
 const router = express.Router();
 const User = require("../models/User");
+
+jdenticon.config = {
+  lightness: {
+    color: [0.23, 0.87],
+    grayscale: [0.38, 0.66]
+  },
+  saturation: {
+    color: 0.74,
+    grayscale: 0.33
+  },
+  backColor: "#86444400"
+};
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -14,7 +29,7 @@ router.get('/', ensureLoggedOut("/main"), (req, res, next) => {
   res.render('index');
 });
 
-router.get("/login", (req, res, next) => { 
+router.get("/login", (req, res, next) => {
   res.render("auth/login", { "message": req.flash("error") });
 });
 
@@ -48,9 +63,6 @@ router.post("/signup", uploadCloud.single('photo'), (req, res, next) => {
   const confirmPass = req.body.confirmpassword;
   const email = req.body.email;
 
-  const imagePath = (req.file === undefined) ? null : req.file.url;
-  
-  const confirmationCode = token;
   if (username === "") {
     res.render("auth/signup", { message: "Indicate an username" });
     return;
@@ -69,49 +81,65 @@ router.post("/signup", uploadCloud.single('photo'), (req, res, next) => {
     return;
   }
 
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
-    }
-  })
-    .then(() => {
-      User.findOne({ email }, "email", (err, user) => {
+  const png = jdenticon.toPng(username, 100);
+  fs.writeFileSync(`${username}.png`, png);
+
+  cloudinary.uploader.upload(`${username}.png`,
+    function (result) {
+
+      fs.unlink(`${username}.png`, function (err) {
+        if (err) throw err;
+        console.log('File deleted!');
+      });
+
+      const imagePath = (req.file === undefined) ? result.url : req.file.url;
+
+      const confirmationCode = token;
+
+      User.findOne({ username }, "username", (err, user) => {
         if (user !== null) {
-          res.render("auth/signup", { message: "The email already exists" });
+          res.render("auth/signup", { message: "The username already exists" });
           return;
         }
       })
-      const salt = bcrypt.genSaltSync(bcryptSalt);
-      const hashPass = bcrypt.hashSync(password, salt);
-
-      const newUser = new User({
-        username,
-        password: hashPass,
-        email,
-        imagePath,
-        confirmationCode
-      });
-
-      newUser.save()
         .then(() => {
-          transporter.sendMail({
-            from: '"You💡Know Corporation" <labsandtests@gmail.com>',
-            to: email,
-            subject: 'Confirmation Email',
-            text: 'Welcome to You💡Know',
-            html: `<p>Welcome to You💡Know</p>
+          User.findOne({ email }, "email", (err, user) => {
+            if (user !== null) {
+              res.render("auth/signup", { message: "The email already exists" });
+              return;
+            }
+          })
+          const salt = bcrypt.genSaltSync(bcryptSalt);
+          const hashPass = bcrypt.hashSync(password, salt);
+
+          const newUser = new User({
+            username,
+            password: hashPass,
+            email,
+            imagePath,
+            confirmationCode
+          });
+
+          newUser.save()
+            .then(() => {
+              transporter.sendMail({
+                from: '"You💡Know Corporation" <labsandtests@gmail.com>',
+                to: email,
+                subject: 'Confirmation Email',
+                text: 'Welcome to You💡Know',
+                html: `<p>Welcome to You💡Know</p>
           <p>Your username is: ${username}</p>
           <a href="http://localhost:3000/confirm/${confirmationCode}">Confirm your email here, for activate your account & can access in our WebSite!<a>
           `,
-          })
-            .then(() => res.redirect("/login"))
-            .catch(err => console.log(err));
+              })
+                .then(() => res.redirect("/login"))
+                .catch(err => console.log(err));
+            })
+            .catch(err => {
+              res.render("auth/signup", { message: "Something went wrong, try again or wait, thanks and sorry for issues" });
+            })
         })
-        .catch(err => {
-          res.render("auth/signup", { message: "Something went wrong, try again or wait, thanks and sorry for issues" });
-        })
-    })
+    }, { public_id: `${username}.png`, folder: 'youknow' })
 });
 
 router.get("/logout", ensureLoggedIn("/login"), (req, res) => {
