cbb319bada85889c6d8917b1a44be92159cb9196
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-28 16:07:48 +0100

    getusers

--- You-Know/routes/postgame.js
+++ You-Know/routes/postgame.js
@@ -4,9 +4,39 @@ const router = express.Router();
 const Category = require("../models/Category");
 const Question = require("../models/Question");
 const User = require("../models/User");
+const Game = require("../models/Game");
 
 const axios = require('axios');
 const lodash = require('lodash');
+
+router.post("/creategame", (req, res, next) => {
+  const userId = req.body.userId;
+  const category = req.body.category;
+  const difficulty = req.body.difficulty;
+  const numberQuestions = req.body.numberquestions;
+
+  User.findById(userId)
+    .then((user) => {
+      const userIdd = user.id
+      let game = {
+        userIdd,
+        category,
+        difficulty,
+        numberQuestions,
+        questionsAnswered: 0,
+        questionsCorrect: 0,
+        questionsFailed: 0,
+        points: 0
+      }
+
+      Game.create(game)
+        .then(created => {
+          res.json({ created })
+        })
+        .catch(err => res.json(err))
+    })
+    .catch(err => res.json(err))
+})
 
 router.post('/checkquestion', (req, res, next) => {
   const questionId = req.body.questionId;
@@ -33,17 +63,17 @@ router.post('/checkquestion', (req, res, next) => {
 
         console.log(pointsToAdd)
 
-        return User.findByIdAndUpdate(userId, { $inc: { points: pointsToAdd} })
-          
+        return User.findByIdAndUpdate(userId, { $inc: { points: pointsToAdd } })
+
 
       } else {
-        res.json({result: false})
+        res.json({ result: false })
       }
-      
+
     })
     .then(result => {
       console.log('sumado');
-      res.json({result: true, points: pointsToAdd})
+      res.json({ result: true, points: pointsToAdd })
     })
     .catch(err => res.json(err))
 
